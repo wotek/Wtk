cf61721d68edd035efbd5665b2d71cfc13ce23cb
Wojciech Zalewski <wotek@wotek.(none)>
2012-04-09 11:58:24 +0200

    Coding style changes.

--- library/App/Wizard.php
+++ library/App/Wizard.php
@@ -11,78 +11,78 @@
 
 class App_Form_Wizard extends App_Form_Wizard_WizardAbstract {
 
-	const OPTION_PAGES = 'pages';
-	const OPTION_NAMESPACE = 'namespace';
-	const OPTION_FORM_ACTION = 'form_action';
-	const OPTION_FORM_CANCEL_ACTION = 'form_cancel_action';
+    const OPTION_PAGES = 'pages';
+    const OPTION_NAMESPACE = 'namespace';
+    const OPTION_FORM_ACTION = 'form_action';
+    const OPTION_FORM_CANCEL_ACTION = 'form_cancel_action';
 
-	private function __construct() {
-		// use factory method
-	}
+    private function __construct() {
+        // use factory method
+    }
 
-	/**
-	 *
-	 * @param array $options
-	 * @return App_Form_Wizard 
-	 */
-	public static function factory($options) {
-		if ($options instanceof Zend_Config)
-			$options = $options->toArray();
+    /**
+     *
+     * @param array $options
+     * @return App_Form_Wizard 
+     */
+    public static function factory($options) {
+        if ($options instanceof Zend_Config)
+            $options = $options->toArray();
 
-		if (!is_array($options))
-			throw new App_Form_Wizard_Exception(
-					'Invalid options. Argument should be a Zend_Config instance 
+        if (!is_array($options))
+            throw new App_Form_Wizard_Exception(
+                    'Invalid options. Argument should be a Zend_Config instance 
 					or array.'
-			);
+            );
 
-		// Create instance
-		$instance = new self;
-		// Set options
-		$instance->setOptions($options);
+        // Create instance
+        $instance = new self;
+        // Set options
+        $instance->setOptions($options);
 
-		return $instance;
-	}
+        return $instance;
+    }
 
-	/**
-	 * Sets options
-	 *
-	 * @param array $options 
-	 */
-	public function setOptions(array $options = array()) {
-		foreach ($options as $option => $value) {
-			$this->setOption($option, $value);
-		}
-		return $this;
-	}
+    /**
+     * Sets options
+     *
+     * @param array $options 
+     */
+    public function setOptions(array $options = array()) {
+        foreach ($options as $option => $value) {
+            $this->setOption($option, $value);
+        }
+        return $this;
+    }
 
-	/**
-	 * Set option
-	 * 
-	 * @param string $option
-	 * @param mixed $value 
-	 */
-	public function setOption($option, $value) {
-		switch ($option) {
-			case self::OPTION_PAGES:
-				$this->setPages($value);
-				break;
+    /**
+     * Set option
+     * 
+     * @param string $option
+     * @param mixed $value 
+     */
+    public function setOption($option, $value) {
+        switch ($option) {
+            case self::OPTION_PAGES:
+                $this->setPages($value);
+                break;
 
-			case self::OPTION_NAMESPACE:
-				$this->setDefaultNamespaceName($value);
-				break;
+            case self::OPTION_NAMESPACE:
+                $this->setDefaultNamespaceName($value);
+                break;
 
-			case self::OPTION_FORM_ACTION:
-				$this->setFormAction($value);
-				break;
+            case self::OPTION_FORM_ACTION:
+                $this->setFormAction($value);
+                break;
 
-			case self::OPTION_FORM_CANCEL_ACTION:
-				$this->setFormCancelAction($value);
-				break;
+            case self::OPTION_FORM_CANCEL_ACTION:
+                $this->setFormCancelAction($value);
+                break;
 
-			default:
-				throw new App_Form_Wizard_Exception("Invalid option given: $option");
-				break;
-		}
-	}
+            default:
+                throw new App_Form_Wizard_Exception("Invalid option given: $option");
+                break;
+        }
+    }
 
 }
